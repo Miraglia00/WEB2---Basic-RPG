3c30115be1e163119ac6270111658aa14337bd7f
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-02-14 23:54:10 +0100

    Added basic initial rendering and entity update rendering

--- Render.js
+++ Render.js
@@ -3,21 +3,66 @@ import Entity from "./Entity.js";
 export default class Render {
     constructor(gridDiv) {
         this.grid = gridDiv;
+        this.entities = [];
+    }
+
+    getRenderedEntities = () => this.entities;
+    getEntityByID = (id) => {
+        for(e of this.entities) {
+            if(e.getEntityID() === id) return e;
+        }
+    }
+
+    
+    isGridOccupied = (x,y,self) => {
+        if(this.entities.length !== 0) {
+           for(let entity of this.entities) {
+               if(entity.getPosition().x === x && entity.getPosition().y === y) {
+                   if(self === entity) {
+                       continue;
+                   }else return true;
+               }
+           }
+        }
+        return false;
     }
 
     renderEntity = (e) => {
-        if(e instanceof Entity) {
-            console.log(e.getPosition())
-            if(e.getPosition().isSpawned === true) {
-                let entity = $("<div class='entity'></div>");
-            
-                $('div.grid-block#' + e.getPosition().x + '-' + e.getPosition().y).append(e.getEntityDiv());
+        if(e instanceof Entity) {           
+            $('div.grid-block#' + e.getPosition().x + '-' + e.getPosition().y).append(e.getEntityDiv());
+            this.entities.push(e);
+        }else{
+            return console.error("Invalid target!" + e instanceof "Entity");
+        }
+    }
+
+    initRenderEntity = (e) => {
+        if(e instanceof Entity) {          
+            e.generatePosition();
+
+            if(!this.isGridOccupied(e.getPosition().x,  e.getPosition().y, e)) {
+                this.renderEntity(e);
             }else{
-                return console.error("Entity not spawned yet!");
+                console.error("initRendering: Grid occupied! Re-rendering entity...");
+                this.initRenderEntity(e);
+            }    
+        }
+    }
+
+    updateEntity = (e) => {
+        if(e instanceof Entity) {
+            if(this.entities.includes(e)) { 
+                if(this.isGridOccupied(e.getPosition().x,  e.getPosition().y, e)) {
+                    console.error("updateEntity: Grid is occupied! Moving not updated!");
+                }else{
+                    $('div.entity#' + e.entityID).remove();
+                    this.renderEntity(e, {x: e.getPosition().x, y: e.getPosition().y });
+                }
+            }else{
+                return console.error("Entity not rendered yet!");
             }
         }else{
             return console.error("Invalid target!" + e instanceof "Entity");
         }
-
     }
 }
