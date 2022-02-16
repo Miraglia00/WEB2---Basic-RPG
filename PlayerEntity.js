d8270e49936d009a4f3ab31ef5ee8fe164175fa8
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-02-16 10:16:25 +0100

    Added actionpoints and fixed spawn position based on entrance door

--- PlayerEntity.js
+++ PlayerEntity.js
@@ -1,8 +1,20 @@
 import Entity from "./Entity.js";
 
 export default class PlayerEntity extends Entity {
-    constructor() {
+    constructor(entraceDoorCoord) {
         super();
-        this.entityDiv.addClass('player'); 
+        this.setBaseHealth(8);
+        this.entityDiv.addClass('player');
+        this.entityType = "Player";
+        this.actionPoints = {
+            base: 2,
+            used: 0
+        };
+        this.setPosition({x:11,y:entraceDoorCoord});
+        this.setIsSpawned(true);
     }
+
+    getActionPoints = () => this.actionPoints;
+    
+
 }
