73220cd64dc439f5b8d72ca3857b2f56b7cdaa84
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-02-18 14:56:32 +0100

    Removed action points

--- PlayerEntity.js
+++ PlayerEntity.js
@@ -6,18 +6,8 @@ export default class PlayerEntity extends Entity {
         this.setBaseHealth(8);
         this.entityDiv.addClass('player');
         this.entityType = "Player";
-        this.actionPoints = {
-            base: 2,
-            used: 0
-        };
         this.setPosition({x:11,y:entraceDoorCoord});
         this.setIsSpawned(true);
-    }
-
-    getActionPoints = () => this.actionPoints;
-
-    setUsedActionPoints = (amount) => {
-        this.actionPoints.used += amount;
     }
     
 
