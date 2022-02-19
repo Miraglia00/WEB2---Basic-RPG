411d4ae71acf5a3c1da5f933869eadf35f035519
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-02-19 12:51:55 +0100

    Set attack to 10, this will be based on difficulty

--- PlayerEntity.js
+++ PlayerEntity.js
@@ -4,6 +4,7 @@ export default class PlayerEntity extends Entity {
     constructor(entraceDoorCoord) {
         super();
         this.setBaseHealth(8);
+        this.attack = 10;
         this.entityDiv.addClass('player');
         this.entityType = "Player";
         this.setPosition({x:11,y:entraceDoorCoord});
