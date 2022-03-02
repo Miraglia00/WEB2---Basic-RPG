d183ad4736204f79dc8816b65899c4dba78cae13
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-02 11:28:23 +0100

    Score saving added

--- save.php
+++ save.php
@@ -1,19 +1,24 @@
 <?php
-
+session_start();
 include('./db/DB.php');
 include('./src/php/UserService.php');
 
 
 
+$redirect = str_replace('save.php', '', $_SERVER['REQUEST_URI']);
 
-if(isset($_POST['name']) && isset($_POST['kills'])) {
+if(isset($_POST['kills']) && isset($_SESSION['user'])) {
   //$username = $db -> real_escape_string($_POST['name']);
   //$review = $db -> real_escape_string($_POST['review']);
   $db = new DB();
-  $user = new UserService("1234", "asdff", $db);
-
+  $id = $_SESSION['user']['id'];
+  $escaped_review = $db->real_escape_string($_POST['review']);
+  $review = ($escaped_review != "") ? "'$escaped_review'" : "NULL";
+  $kills = $db->real_escape_string($_POST['kills']);
+  $db->execute_query("INSERT INTO web2_saves(user_id, review, kills, saved_at) VALUES ('$id', $review, '$kills', NOW())");
+  $_SESSION['success_message'] = "Your data saved to the Hall Of Fame! Check your status at the Leaderboard.";
+  header('Location: '.$redirect);
 }else{
-  $redirect = str_replace('save.php', '', $_SERVER['REQUEST_URI']);
   header('Location: '.$redirect);
 }
 ?>
