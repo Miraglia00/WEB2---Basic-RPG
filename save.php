9e755a6be69239fd0cdc04a22dc1f9a5ae18f2f9
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-02 11:45:40 +0100

    Turn save

--- save.php
+++ save.php
@@ -7,7 +7,7 @@ include('./src/php/UserService.php');
 
 $redirect = str_replace('save.php', '', $_SERVER['REQUEST_URI']);
 
-if(isset($_POST['kills']) && isset($_SESSION['user'])) {
+if(isset($_POST['kills']) && isset($_POST['turns']) && isset($_SESSION['user'])) {
   //$username = $db -> real_escape_string($_POST['name']);
   //$review = $db -> real_escape_string($_POST['review']);
   $db = new DB();
@@ -15,7 +15,8 @@ if(isset($_POST['kills']) && isset($_SESSION['user'])) {
   $escaped_review = $db->real_escape_string($_POST['review']);
   $review = ($escaped_review != "") ? "'$escaped_review'" : "NULL";
   $kills = $db->real_escape_string($_POST['kills']);
-  $db->execute_query("INSERT INTO web2_saves(user_id, review, kills, saved_at) VALUES ('$id', $review, '$kills', NOW())");
+  $turns = $db->real_escape_string($_POST['turns']);
+  $db->execute_query("INSERT INTO web2_saves(user_id, review, kills, turns, saved_at) VALUES ('$id', $review, '$kills', '$turns', NOW())");
   $_SESSION['success_message'] = "Your data saved to the Hall Of Fame! Check your status at the Leaderboard.";
   header('Location: '.$redirect);
 }else{
